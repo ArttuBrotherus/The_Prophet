cb8a71f206a5c94ac3b48d2176e991b507b009e7
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2018-12-10 18:18:33 +0100

    Coins work, simplistically

--- Pantless_Baby_Game/Assets/Game_Master_Spt.cs
+++ Pantless_Baby_Game/Assets/Game_Master_Spt.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_Master_Spt : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int points;
+
+    public Text pointsText;
+
+    void Update()
+    {
+        pointsText.text = "Points: " + points;
+    }
+
 }
--- Pantless_Baby_Game/Assets/Player.cs
+++ Pantless_Baby_Game/Assets/Player.cs
@@ -3,25 +3,24 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	[System.Serializable]
-	public class PlayerStats {
-		public int Health = 100;
+    private Game_Master_Spt gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<Game_Master_Spt>();
+    }
+
+    void Update () {
+		//_
 	}
 
-	public PlayerStats playerStats = new PlayerStats();
-
-	public int fallBoundary = -20;
-
-	void Update () {
-		if (transform.position.y <= fallBoundary)
-			DamagePlayer (9999999);
-	}
-
-	public void DamagePlayer (int damage) {
-		playerStats.Health -= damage;
-		if (playerStats.Health <= 0) {
-			GameMaster.KillPlayer(this);
-		}
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Currency"))
+        {
+            Destroy(collision.gameObject);
+            gm.points += 1;
+        }
+    }
 
 }
