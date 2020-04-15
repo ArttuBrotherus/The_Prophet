d079abbd840fa390a3478dd99d6f27ab3289bd2b
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2020-04-15 20:24:59 +0200

    Teeth wheel and death (initial)

--- Pantless_Baby_Game/Assets/Player.cs
+++ Pantless_Baby_Game/Assets/Player.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     private Game_Master_Spt gm;
+    private bool dead = false;
 
     private void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<Game_Master_Spt>();
     }
 
-    void Update () {
-		//_
-	}
+    void Update() {
+        if (dead)
+        {
+            transform.Translate(0,7 * Time.deltaTime, 0);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -21,6 +25,15 @@ public class Player : MonoBehaviour {
             Destroy(collision.gameObject);
             gm.points += 1;
         }
+    }
+
+    public void Die (){
+        if (dead) return;
+        dead = true;
+        Debug.Log("DEATH");
+        this.GetComponent<CharacterController>().enabled = false;
+        this.GetComponent<PhysicsScript>().enabled = false;
+        transform.Rotate(0,0,180f);
     }
 
 }
--- Pantless_Baby_Game/Assets/teeth_wheel_script.cs
+++ Pantless_Baby_Game/Assets/teeth_wheel_script.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class teeth_wheel_script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Rotate(0,0,-360f * Time.deltaTime);
 	}
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("DEATH");
+        var target = col.gameObject;
+        if (target.tag == "Player") {
+            var player = target.GetComponent<Player>();
+            player.Die();
+        }
     }
 }
