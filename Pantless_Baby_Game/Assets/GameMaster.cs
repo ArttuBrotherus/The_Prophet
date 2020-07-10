a134df9d680e182f2ee45cadf31180cd0d9451c2
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2020-07-10 16:00:18 +0200

    Going through blocks, need a way to distinguish between all children

--- Pantless_Baby_Game/Assets/GameMaster.cs
+++ Pantless_Baby_Game/Assets/GameMaster.cs
@@ -3,12 +3,35 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 
+    GameObject[] Sup_Edges_L;
+    public GameObject floor;
+    public GameObject wall;
+
 	public static void KillPlayer (Player player) {
 		Destroy (player.gameObject);
 	}
 
-    private void Start()
+    private void Start ()
     {
-        //
+        CreatePhysicalBlocks();
+    }
+
+    void CreatePhysicalBlocks()
+    {
+        Sup_Edges_L = GameObject.FindGameObjectsWithTag("S_Edge_L");
+        foreach(GameObject edge_l in Sup_Edges_L)
+        {
+            GoThroughBlocks(edge_l);
+        }
+    }
+
+    void GoThroughBlocks(GameObject edge_l)
+    {
+        var floorChild = Instantiate(floor);
+        var wallChild = Instantiate(wall);
+        floorChild.transform.parent = edge_l.transform;
+        wallChild.transform.parent = edge_l.transform;
+
+        //floorChild.transform. = 0.95f;
     }
 }
