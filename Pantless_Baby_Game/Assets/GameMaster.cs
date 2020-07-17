ef876956bfffdf9e7ef05bd1111bb903bd1f2265
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2020-07-17 14:15:46 +0200

    Removed Dict. BoxCollider2D accessed in a function

--- Pantless_Baby_Game/Assets/GameMaster.cs
+++ Pantless_Baby_Game/Assets/GameMaster.cs
@@ -5,33 +5,9 @@ using System.Collections.Generic;
 
 public class GameMaster : MonoBehaviour {
 
-    GameObject[] Sup_Edges_L;
-    public GameObject floor;
-    public GameObject wall;
-
-    Dictionary<Transform, GameObject> Physical_Materials = new Dictionary<Transform, GameObject>();
 
     public static void KillPlayer (Player player) {
 		Destroy (player.gameObject);
 	}
 
-    private void Start ()
-    {
-        CreatePhysicalBlocks();
-    }
-
-    void CreatePhysicalBlocks()
-    {
-        Sup_Edges_L = GameObject.FindGameObjectsWithTag("S_Edge_L");
-        foreach(GameObject edge_l in Sup_Edges_L)
-        {
-            GoThroughBlocks(edge_l);
-        }
-    }
-
-    void GoThroughBlocks(GameObject edge_l)
-    {
-        Physical_Materials.Add(edge_l.transform, Instantiate(floor, edge_l.transform.position, Quaternion.identity));
-        Physical_Materials.Add(edge_l.transform, Instantiate(wall, edge_l.transform.position, Quaternion.identity));
-    }
 }
