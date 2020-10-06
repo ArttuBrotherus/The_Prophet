62f7f9c2f4bca125885040294d2c13162f885804
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2020-10-06 19:55:45 +0200

    Plasma trap travels on a rail

---
+++ Pantless_Baby_Game/Assets/Plasma_Script.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MovementStep
+{
+    public Vector2 startPoint;
+    public Vector2 direction; //eg. [+1,0]
+    public float distance;
+}
+
+public class Plasma_Script : MonoBehaviour
+{
+    public static MovementStep[][] tracks =
+        new MovementStep[][] {
+            new MovementStep[]
+            {
+                new MovementStep{startPoint = new Vector2(67f, -21f), direction = new Vector2(0, -1), distance = 2f },
+                new MovementStep{startPoint = new Vector2(67f, -23f), direction = new Vector2(0, 1), distance = 2f},
+                new MovementStep{startPoint = new Vector2(67f, -21f), direction = new Vector2(-1, 0), distance = 4f},
+                new MovementStep{startPoint = new Vector2(63f, -21f), direction = new Vector2(1, 0), distance = 4f},
+            },
+            new MovementStep[]
+            {
+                new MovementStep{startPoint = new Vector2(67f, -21f), direction = new Vector2(0, -1), distance = 2f },
+                new MovementStep{startPoint = new Vector2(67f, -23f), direction = new Vector2(0, 1), distance = 2f},
+                new MovementStep{startPoint = new Vector2(67f, -21f), direction = new Vector2(-1, 0), distance = 4f},
+                new MovementStep{startPoint = new Vector2(63f, -21f), direction = new Vector2(1, 0), distance = 4f},
+            }
+        };
+
+    // Dictionary<string, object>[] track2;  // alternative data structure
+
+    public int trackIndex; 
+
+    int Step = 0;
+    float travelledDistance = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var track = tracks[trackIndex];
+        var currentStep = track[Step];
+        var x = currentStep.startPoint.x + currentStep.direction.x * travelledDistance;
+        var y = currentStep.startPoint.y + currentStep.direction.y * travelledDistance;
+        transform.position = new Vector2(x, y);
+        travelledDistance += Time.deltaTime;
+
+        if(travelledDistance > currentStep.distance)
+        {
+            Step++;
+            if (Step > track.Length - 1) Step = 0;
+            travelledDistance = 0;
+        }
+    }
+}
