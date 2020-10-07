70071565e72b87b322456e5f1bed183292dd254b
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2020-10-07 19:13:18 +0200

    Plasma Trap - Serialize

--- Pantless_Baby_Game/Assets/Plasma_Script.cs
+++ Pantless_Baby_Game/Assets/Plasma_Script.cs
@@ -1,35 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public struct MovementStep
 {
-    public Vector2 startPoint;
-    public Vector2 direction; //eg. [+1,0]
-    public float distance;
+    [SerializeField] public Vector2 startPoint;
+    [SerializeField] public Vector2 direction; //eg. [+1,0]
+    [SerializeField] public float distance;
 }
 
 public class Plasma_Script : MonoBehaviour
 {
-    public static MovementStep[][] tracks =
-        new MovementStep[][] {
-            new MovementStep[]
-            {
-                new MovementStep{startPoint = new Vector2(67f, -21f), direction = new Vector2(0, -1), distance = 2f },
-                new MovementStep{startPoint = new Vector2(67f, -23f), direction = new Vector2(0, 1), distance = 2f},
-                new MovementStep{startPoint = new Vector2(67f, -21f), direction = new Vector2(-1, 0), distance = 4f},
-                new MovementStep{startPoint = new Vector2(63f, -21f), direction = new Vector2(1, 0), distance = 4f},
-            },
-            new MovementStep[]
-            {
-                new MovementStep{startPoint = new Vector2(65f, -23f), direction = new Vector2(-1, 0), distance = 2f },
-                new MovementStep{startPoint = new Vector2(63f, -23f), direction = new Vector2(1, 0), distance = 2f},
-            }
-        };
 
-    // Dictionary<string, object>[] track2;  // alternative data structure
-
-    public int trackIndex; 
+    public MovementStep[] track; 
 
     int Step = 0;
     float travelledDistance = 0;
@@ -43,7 +28,6 @@ public class Plasma_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var track = tracks[trackIndex];
         var currentStep = track[Step];
         var x = currentStep.startPoint.x + currentStep.direction.x * travelledDistance;
         var y = currentStep.startPoint.y + currentStep.direction.y * travelledDistance;
@@ -58,9 +42,4 @@ public class Plasma_Script : MonoBehaviour
         }
     }
 
-    //Rail end points have Box-Triggers, the time the trap needs to travel from
-    //A to B is kept track of. The trap needs some time to go this distance.
-    //When leaving some point, you switch off the collider of the trap,
-    //then you switch it back on to find out in which direction the
-    //current part of the trail sets the trap towards.
 }
