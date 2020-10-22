4aa52058fda959841faa7b46a99fb363d3c7411a
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2020-10-22 15:23:25 +0200

    Enemy goes back and forth, TurnSpikes only visible in Editor

--- Pantless_Baby_Game/Assets/Scripts/EnemyBackAndForth.cs
+++ Pantless_Baby_Game/Assets/Scripts/EnemyBackAndForth.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class EnemyBackAndForth : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;   
+    Rigidbody2D body;
+
+    int goingRight = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = new Vector2(0.75f * goingRight, body.velocity.y);
     }
 
     public void Turn()
@@ -24,10 +28,12 @@ public class EnemyBackAndForth : MonoBehaviour
         if (spriteRenderer.flipX)
         {
             spriteRenderer.flipX = false;
+            goingRight = 1;
         }
         else
         {
             spriteRenderer.flipX = true;
+            goingRight = -1;
         }
     }
 }
--- Pantless_Baby_Game/Assets/Scripts/GameMaster.cs
+++ Pantless_Baby_Game/Assets/Scripts/GameMaster.cs
@@ -5,9 +5,19 @@ using System.Collections.Generic;
 
 public class GameMaster : MonoBehaviour {
 
+    GameObject[] invisibleInGame;
 
     public static void KillPlayer (Player player) {
 		Destroy (player.gameObject);
 	}
+
+    private void Awake()
+    {
+        invisibleInGame = GameObject.FindGameObjectsWithTag("HiddenIn-Game");
+        foreach (GameObject hideItem in invisibleInGame)
+        {
+            hideItem.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
 
 }
