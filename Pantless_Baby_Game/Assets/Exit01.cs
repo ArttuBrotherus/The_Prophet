b24b3bc6b3781562614f9a810b76eb9138ffcd3b
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2021-03-18 22:59:04 +0100

    Primitive exit

---
+++ Pantless_Baby_Game/Assets/Exit01.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit01 : MonoBehaviour
+{
+
+    private bool readyExit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (readyExit)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        readyExit = true;
+    }
+
+    private void OnTriggerExit2D()
+    {
+        readyExit = false;
+    }
+}
