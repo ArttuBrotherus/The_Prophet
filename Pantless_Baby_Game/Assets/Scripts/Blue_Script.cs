4f8f0cc707520969c06df5269879ab933407415a
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2021-03-25 12:22:37 +0100

    Main menu, working on Blue Plate

---
+++ Pantless_Baby_Game/Assets/Scripts/Blue_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blue_Script : MonoBehaviour
+{
+    public float cycleTime;
+    float progress = 0f;
+    float dir = 1f;
+
+    SpriteRenderer SRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progress = progress + dir * Time.deltaTime;
+        
+        if(progress > 0.5f * cycleTime && progress < 0)
+        {
+            dir = dir == 1 ? -1 : 1;
+        }
+        else
+        {
+            SRenderer.color = new Color(1f, 110f, 153f, progress / (0.5f * cycleTime));
+        }
+    }
+}
