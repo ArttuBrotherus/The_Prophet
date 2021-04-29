c60e08380888dcc302607789ecfbf527e2fe4f67
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2021-04-29 20:36:51 +0200

    Start menu button with no function

---
+++ Pantless_Baby_Game/Assets/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start of the button");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startTheGame()
+    {
+        Debug.Log("ååå");
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("button mouse down");
+    }
+}
