2acc9d8b34ce25798bbf0b06efd236eac28d989b
Arttu Tapio Brotherus <arttu.brotherus@gmail.com>
2021-05-18 16:31:51 +0200

    First kangaroos

---
+++ Pantless_Baby_Game/Assets/Scripts/Kangoroo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kangoroo : MonoBehaviour
+{
+
+    public Vector2 jumpVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(jumpVel == new Vector2 (0,0))
+        {
+            //change the two values to default
+
+            jumpVel = new Vector2(1, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
